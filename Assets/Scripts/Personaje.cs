1d2171b0b9cc51f167c9d3e5c28a47c4f1780ae9
Franco Ingrassia PC <fran.ingrassiacarretero@gmail.com>
2022-05-23 23:25:43 -0300

    cambios tansform

--- Assets/Scripts/Personaje.cs
+++ Assets/Scripts/Personaje.cs
@@ -5,19 +5,26 @@ using UnityEngine;
 public class Personaje : MonoBehaviour
 {
     public int puntos = 0;
+    public Vector3 coordenadas;
     private void Awake()
     {
         Debug.Log("Soy el Awake");
+        coordenadas = new Vector3(1, 0, 0);
     }
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Soy el Start");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Soy el Update");
+        transform.GetComponent<SpriteRenderer>().flipY = true;
+        //transform.position = coordenadas;
+
+        //coordenadas = new Vector3(transform.position.x + 0.001f, transform.position.y + 0.001f, 0);
+        //transform.Rotate(new Vector3(0, 0, -1));
     }
 }
